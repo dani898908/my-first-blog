ad262ca4a470de8f8a2c0ac8cdc4f0d16cb3d31e
daniela <danielabonillac@hotmail.com>
2020-09-28 22:30:40 +0200

    cambios root

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/home/danielabonilla89/danielabonilla89.pythonanywhere.com/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
